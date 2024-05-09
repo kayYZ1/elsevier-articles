862438c683a0c8d87ef7c301c6dc92c93826e57c
Kacper <overek7133@gmail.com>
2024-05-09 13:35:52 +0200

    xd

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ api_key = "7e9c35d9d78f7e34567e6e4eef3ce6c1"
 base_url = "https://api.elsevier.com/content/search/sciencedirect"
 
 query = "(KEY artificial intelligence)"
-url = f"{base_url}?apiKey={api_key}&query={query}&date=2023&count=100"
+url = f"{base_url}?apiKey={api_key}&query={query}&date=2016&count=100"
 
 response = requests.get(url)
 
@@ -27,27 +27,31 @@ for doi in doi_list:
     article_retrieval_response = requests.get(article_retrieval_url)
     if article_retrieval_response.status_code == 200:
         data = article_retrieval_response.json()
+
         article = data["full-text-retrieval-response"]
         article_core_data = article["coredata"]
-
         articleOAFlag = article_core_data["openaccess"]
+
         if articleOAFlag == "1":
-            imageCount = 0
+            if "objects" in article:
+                articleObjects = article["objects"]["object"]
+                imageCount = 0
 
-            articleObjects = article["objects"]["object"]
-            for object in articleObjects:
-                if (
-                    object["@category"] == "standard"
-                    and object["@mimetype"] == "image/jpeg"
-                ):
-                    imageCount += 1
+                for object in articleObjects:
+                    if (
+                        object["@category"] == "standard"
+                        and object["@mimetype"] == "image/jpeg"
+                    ):
+                        imageCount += 1
 
-            valid_article = {
-                "Title": article_core_data["dc:title"],
-                "Date": article_core_data["prism:coverDisplayDate"],
-                # "Text": article["originalText"],
-                "Images": imageCount,
-            }
+                valid_article = {
+                    "Title": article_core_data["dc:title"],
+                    "Date": article_core_data["prism:coverDisplayDate"],
+                    # "Text": article["originalText"],
+                    "Images": imageCount,
+                }
+            else:
+                continue
 
             articles[doi] = valid_article
 
@@ -61,12 +65,11 @@ articles_json = json.dumps(articles, indent=4)
 
 print(articles_json)
 
-"""
-art2013 = "articles_2013.json"
-art2023 = "articles_2023.json"
-"""
+# art_txt_2013 = "articles_txt_2013.json"
+# art_txt_2023 = "articles_txt_2023.json"
 
+art_img_2013 = "articles_img_2013.json"
 art_img_2023 = "articles_img_2023.json"
 
-with open(f"files/{art_img_2023}", "w") as file:
+with open(f"files/{art_img_2013}", "w") as file:
     file.write(articles_json)
